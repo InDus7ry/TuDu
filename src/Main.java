54c3f326030700ab4bb0b0640b17c0db1d645099
InDus7ry <39075547+InDus7ry@users.noreply.github.com>
2020-03-10 07:46:23 -0600

    f

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,14 @@
 //@author Gabe Wong
 //@version %I%
 
-import java.awt.EventQueue;
+import java.awt.Dimension;
 import java.util.ArrayList;
+import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
 		ArrayList<Task> tasks;
 		ArrayList<Event> events;
 		GUI mgui = new GUI();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					mgui.initialize();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
 	}
 }
